252bafdc63ab0a450309e92b95c493d24f8ef8d9
Andy Gebby <agebby@windstream.net>
2013-07-10 13:15:01 -0400

    Added each lesson for 2,3,4

---
+++ Lesson3-CreatePlayers/js/game.js
@@ -0,0 +1,12 @@
+
+var CANVAS_WIDTH = 480;
+var CANVAS_HEIGHT = 320;
+var counter = 0;
+var canvasElement = $("<canvas width='" + CANVAS_WIDTH + 
+                      "' height='" + CANVAS_HEIGHT + "'></canvas>");
+var canvas = canvasElement.get(0).getContext("2d");
+
+
+canvasElement.appendTo('#gameContainer');
+
+
